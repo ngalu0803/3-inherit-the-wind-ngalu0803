1a29123f3f447599b273c6152781d16b7de0bc57
ngalu0803 <55891417+ngalu0803@users.noreply.github.com>
2020-02-20 10:20:48 -0500

    Created Person Model

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,55 @@
 public class Person {
+    private String ID;
+    private String firstName;
+    private String lastName;
+    private String title;
+    private int YOB;
 
+    public Person(String ID, String firstName, String lastName, String title, int YOB) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.title = title;
+        this.YOB = YOB;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYOB(int YOB) {
+        this.YOB = YOB;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYOB() {
+        return YOB;
+    }
 }
